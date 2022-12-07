18b41487895693ea53b902097f20433349f9291e
Sodiq Farhan <sodiqfarhan360@gmail.com>
2022-12-07 16:11:59 +0100

    Modify project format and integrate with frontend

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express');
 const connectDB = require('./config/dbConfig')
 require('dotenv').config();
@@ -6,12 +7,11 @@ const shortid = require('shortid');
 
 const app = express();
 app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs');
 
 app.get('/', async (req, res) => {
-    const allUrls = await shortUrlModel.find()
-    res.render('index', {allUrls})
-    // res.send("Welcome")
+    res.render('index', {urlFound: null})
 })
 
 app.post('/short-urls', async (req, res) => {
@@ -20,8 +20,16 @@ app.post('/short-urls', async (req, res) => {
         fullUrl: req.body.fullUrl,
         shortUrl: shortUrl
     }
-    await shortUrlModel.create(newUrl)
-    res.redirect('/');
+    await shortUrlModel.create(newUrl);
+    // res.redirect('/');
+    res.render('short', {newUrl})
+})
+
+app.post('/short-details', async (req, res) => {
+    const urlFound = await shortUrlModel.findOne({shortUrl: req.body.checkShort})
+    if (!urlFound) return res.render('details', {urlFound: null, message: `The url ${req.body.checkShort} can't be found!`})
+    
+    res.render('details', {urlFound: urlFound, message: null})
 })
 
 app.get('/:shorturl', async (req, res) => {
